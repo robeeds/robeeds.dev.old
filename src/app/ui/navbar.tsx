4675d8b6862d45edc54ad1be4465acdd26f67a0b
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-10-01 12:53:50 -0700

    Create basic routing

---
+++ src/app/ui/navbar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+// import NavLinks from '@/app/ui/dashboard/nav-links';
+import logo from '@/app/ui/logo.png';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+export default function NavBar() {
+    return (
+        <div>
+            This is the navbar
+        </div>
+    )
+}
