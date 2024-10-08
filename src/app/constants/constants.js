087616827b7411e58693cc5ca03b190eb064aed2
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-10-08 13:30:37 -0700

    Added constants file

---
+++ src/app/constants/constants.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    { 
+        name: 'home', 
+        href: '/',
+    },
+    {
+        name: 'about',
+        href: '#about',
+    },
+    {
+        name: 'blog',
+        href: '/blog',
+    },
+    {
+        name: 'portfolio',
+        href: '#portfolio',
+    }, 
+    {
+        name: 'contact',
+        href: '#contact',
+    },
+];
