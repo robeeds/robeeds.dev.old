2d8db0680b8cf02355f8bfbbc4630f2f9507203a
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-10-16 14:16:07 -0700

    Added title to Blog Section

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,14 @@
 export default function Blog() {
     return (
-        <div className="flex flex-1">Blog</div>
+        <div className="flex flex-1 justify-center p-4">
+
+            <div className="flex flex-col items-center">
+                <p className="text-[48px]">
+                    Coming Soon!
+                </p>
+            </div>
+
+
+        </div>
     )
 }
