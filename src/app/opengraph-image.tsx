6e40099907c6e42c4f9b3f0463945aef4193ab66
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-10-23 09:25:33 -0700

    Set up for dynamic og image generation

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,42 +1,71 @@
 import { ImageResponse } from 'next/og'
+ 
+export const runtime = 'edge'
+ 
+// Image metadata
+export const alt = 'My Portfolio'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const title = 'My Portfolio'
+export const description = 'Happy Coding!'
+ 
+export const contentType = 'image/png'
+ 
+// Image generation
+export default async function Image() {
 
-export const config = {
-  runtime: 'edge',
-};
+  // Font
+  const poppinsSemibold = fetch(new URL('./fonts/Poppins-Semibold.ttf', import.meta.url)).then((res) => res.arrayBuffer())
+ 
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          backgroundImage: 'url(http://robeeds.tech/images/og-bg.png)',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          color: '#E5E9E9',
+          paddingTop: '50px',
+        }}
+      >
 
-const OpenGraph = async (request: NextRequest) => {
-  const title = 'RobeeDS';
-  const description = 'Full Stack Developer';
-
-  try {
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            backgroundColor: 'black',
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white',
+        <p 
+          style={{ 
+            fontSize: 96, 
+            margin: 0, 
+            paddingLeft: '22px',
           }}
-        >
-          <div style={{ fontSize: '48px' }}>{title}</div>
-          <div style={{ fontSize: '16px' }}>{description}</div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    );
-  } catch (error) {
-    return new Response(error.message || error.toString(), {
-      status: 500,
-    });
-  }
-};
+        >{title}</p>
 
-export default OpenGraph;
+        <p 
+          style={{ 
+            fontSize: 54, 
+            margin: 0, 
+            paddingLeft: '25px', 
+          }}
+        >{description}</p>
+
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: 'Poppins',
+          data: await poppinsSemibold,
+          style: 'normal',
+          weight: 400,
+        }
+      ]
+    }
+  )
+}
