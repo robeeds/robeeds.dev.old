c5f9e799adb585cab1d29bd62fd3f6e6e77e0604
robeeds <71895118+robeeds@users.noreply.github.com>
2024-11-01 20:54:06 -0700

    Added 404 Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+ 
+export default function NotFound() {
+  return (
+    <div className='flex flex-1 flex-col items-center justify-center p-4'>
+
+        {/* Error Message */}
+        <p className='p-4'>404: Sorry! We couldn't find that page</p>
+        <motion.button 
+            className='bg-blue text-black px-4 py-2.5 rounded-full'
+            whileHover={{ scale: 1.1 }}
+        >
+            <Link href='/'>Return Home</Link>
+        </motion.button>
+    </div>
+  )
+}
