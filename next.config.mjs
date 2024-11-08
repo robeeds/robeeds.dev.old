58507152ab530e2d64456a9ae6540a4168538778
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-11-08 15:20:32 -0800

    Add MDX support for blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    transpilePackages: ['next-mdx-remote'],
+
+};
+
+const withMDX = createMDX({
+    // Add markdown plugins here
+    options: {
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter]
+    }
+})
+
+export default withMDX(nextConfig);
