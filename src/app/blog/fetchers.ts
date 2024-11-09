990cd1f9abb6802246fc6621be66df56ffa282b6
robeeds <71895118+robeeds@users.noreply.github.com>
2024-11-08 17:49:21 -0800

    Added error handling for file checking

--- src/app/blog/fetchers.ts
+++ src/app/blog/fetchers.ts
@@ -2,14 +2,24 @@ import fs from "fs"
 import path from "path"
 import { compileMDX } from 'next-mdx-remote/rsc'
 
+import { notFound } from "next/navigation"
+
 const contentDir = path.join(process.cwd(), "/src/app/blog/_content")
 
 export async function getBlogBySlug(slug: string) {
     const fileName = slug + ".mdx"
     const filePath = path.join(contentDir, fileName)
-    const fileContent = fs.readFileSync(filePath, "utf8")
+    let fileContent = ''
+
+    if(!fs.existsSync(filePath)) {
+        notFound()
+    }  else {
+        fileContent = fs.readFileSync(filePath, "utf8")
+    }
+
     const { frontmatter, content } = await compileMDX<{
         title: string
+        description: string
         author: string
         publishDate: string
     }>({
