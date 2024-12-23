8d0faa9d227204e4284341b88afad2374c8673a6
robeeds <71895118+robeeds@users.noreply.github.com>
2024-12-23 12:00:37 -0800

    Update font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Poppins } from 'next/font/google'
 import NavBar from "./ui/navbar";
 import Footer from "./ui/footer";
 
-const poppins = Poppins({
+export const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
 })
